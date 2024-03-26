d6eec77ca2e085fc79d6694f21b6671f8e44e003
Shehab Mokhtar <shahbeldinmokhtar@gmail.com>
2024-03-26 08:33:58 +0300

    Nav bar

--- lib/modules/layout/customer_layout/presentation/views/widgets/customer_navbar.dart
+++ lib/modules/layout/customer_layout/presentation/views/widgets/customer_navbar.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:market_app/core/services/utils.dart';
-import 'package:market_app/modules/layout/customer_layout/presentation/views/widgets/bottom_nav_item.dart';
+import 'package:market_app/core/styles/colors.dart';
+import 'package:market_app/core/styles/sizes.dart';
 
 // ignore: must_be_immutable
 class CustomerNavBar extends StatelessWidget {
   int currentIndex;
-  Function onTap;
+  Function(int value) onTap;
   CustomerNavBar({
     super.key,
     required this.currentIndex,
@@ -14,32 +16,59 @@ class CustomerNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: (value) {
-        AppUtilities.vibration();
-        onTap(value);
-      },
-      items: bottomnavigationBarItems,
+    return Container(
+      color: Colors.grey[100],
+      child: Padding(
+        padding: const EdgeInsetsDirectional.only(
+          top: 10,
+          start: 10,
+          end: 10,
+          bottom: 12,
+        ),
+        child: GNav(
+            selectedIndex: currentIndex,
+            onTabChange: (value) {
+              AppUtilities.vibration(duration: 15);
+              onTap(value);
+            },
+            rippleColor: AppColors.primaryColor
+                .withOpacity(.2), // tab button ripple color when pressed
+            tabBorderRadius: AppSizes.borderRadius,
+            // tabBorder: Border.all(
+            //     color: Colors.black12, width: 0.5), // tab button border
+            // tab animation curves
+            duration:
+                const Duration(milliseconds: 250), // tab animation duration
+            gap: 5, // the tab button gap between icon and text
+
+            color: Theme.of(context).iconTheme.color, // unselected icon color
+            activeColor: AppColors.primaryColor, // selected icon and text color
+            iconSize: AppSizes.iconSize(context), // tab button icon size
+            tabBackgroundColor: AppColors.primaryColor
+                .withOpacity(.1), // selected tab background color
+            padding: const EdgeInsets.symmetric(
+                horizontal: 15, vertical: 7), // navigation bar padding
+            tabs: bottomNavBarItems),
+      ),
     );
   }
 
-  List<BottomNavigationBarItem> bottomnavigationBarItems = [
-    customBottomNavigationBarItem(
+  List<GButton> bottomNavBarItems = const [
+    GButton(
       icon: Icons.home_outlined,
-      label: 'Home',
+      text: 'Home',
     ),
-    customBottomNavigationBarItem(
+    GButton(
       icon: Icons.search_outlined,
-      label: 'Search',
+      text: 'Search',
     ),
-    customBottomNavigationBarItem(
+    GButton(
       icon: Icons.favorite_outline,
-      label: 'Favorites',
+      text: 'Favorites',
     ),
-    customBottomNavigationBarItem(
+    GButton(
       icon: Icons.person_outline,
-      label: 'Profile',
-    ),
+      text: 'Profile',
+    )
   ];
 }
